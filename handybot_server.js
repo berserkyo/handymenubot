7be48c386e897cadf8a49e85ccb99079a63fcba5
berserkyo <berserkyo@naver.com>
2018-08-15 23:39:59 +0900

    오띠모푸드 메뉴클릭시 동기화 처리되지않던 메시지버그 수정

--- handybot_server.js
+++ handybot_server.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3').verbose();
-var dateutil = require('date-utils');
+//var dateutil = require('date-utils');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -36,33 +36,31 @@ function setToday(str) {
     var dayOfWeek = new Date().getDay();
     todayLabel = week[dayOfWeek];
 
-    if (str == 'ottimo') {
+    if (str == 'ottimoJungsik') {
         var today = new Date();
         var dd = today.getDate();
         var year = today.getFullYear();
         var mm = today.getMonth() + 1;
 
-        //오띠모푸드 중식/석식 시간체크
-        dateutil = new Date();
-        var ottimo_time = dateutil.toFormat('HH24:MI');
+        //오띠모푸드 중식 설정값 셋팅
+        today_text_name = year + "_" + mm + "_" + dd + "_jungsik.txt";
+        today_ottimo_message = year + "/" + mm + "/" + dd + " (" + todayLabel + ") (중식)";
+        today_image_name = year + "_" + mm + "_" + dd + "_jungsik.jpg";
+        ottimo_text = '아직 ' + today_ottimo_message + " 메뉴가 공지되지 않았습니다. 오띠모푸드 중식 메뉴는 오전 11시 ~ 12시 사이에 업데이트 됩니다.\n식당사정에 의해 공지되지 않을수도 있습니다.";
+        today_image_url = "http://13.209.234.250:3000/no_menu.jpg";
+    }else if(str == 'ottimoSuksik'){
+        var today = new Date();
+        var dd = today.getDate();
+        var year = today.getFullYear();
+        var mm = today.getMonth() + 1;
 
-        if(ottimo_time >= '00:00' && ottimo_time <= '16:00'){
-          today_text_name = year + "_" + mm + "_" + dd + "_jungsik.txt";
-          today_ottimo_message = year + "/" + mm + "/" + dd + " (" + todayLabel + ") (중식)";
-          today_image_name = year + "_" + mm + "_" + dd + "_jungsik.jpg";
-
-          ottimo_text = '아직 ' + today_ottimo_message + " 메뉴가 공지되지 않았습니다. 오띠모푸드 중식 메뉴는 오전 11:00 ~ 12:00 사이에 업데이트 됩니다.";
-          today_image_url = "http://13.209.234.250:3000/no_menu.jpg";
-        }else{
-          today_text_name = year + "_" + mm + "_" + dd + "_suksik.txt";
-          today_ottimo_message = year + "/" + mm + "/" + dd + " (" + todayLabel + ") (석식)";
-          today_image_name = year + "_" + mm + "_" + dd + "_suksik.jpg";
-
-          ottimo_text = '아직 ' + today_ottimo_message + " 메뉴가 공지되지 않았습니다. 오띠모푸드 석식 메뉴는 오후 17:00 ~ 18:00 사이에 업데이트 됩니다.";
-          today_image_url = "http://13.209.234.250:3000/no_menu2.jpg";
-        }
-
-    } else {
+        //오띠모푸드 석식 설정값 셋팅
+        today_text_name = year + "_" + mm + "_" + dd + "_suksik.txt";
+        today_ottimo_message = year + "/" + mm + "/" + dd + " (" + todayLabel + ") (석식)";
+        today_image_name = year + "_" + mm + "_" + dd + "_suksik.jpg";
+        ottimo_text = '아직 ' + today_ottimo_message + " 메뉴가 공지되지 않았습니다. 오띠모푸드 석식 메뉴는 오후 5시 ~ 6시 사이에 업데이트 됩니다.\n식당사정에 의해 공지되지 않을수도 있습니다.";
+        today_image_url = "http://13.209.234.250:3000/no_menu2.jpg";
+    }else {
         var today = new Date();
         var year = today.getFullYear();
         var mm = today.getMonth() + 1;
@@ -229,49 +227,68 @@ app.post('/message', function(req, res) {
 
     if (_obj.type == "text") {
         if (_obj.content == "오띠모푸드") {
-            setToday('ottimo');
+          console.log(today + '오띠모푸드 클릭->' + _obj.user_key);
+          res_object = {
+              "message": {
+                  "text": '어느 시간대의 메뉴가 궁금하세요?'
+              },
+              "keyboard": {
+                  "type": "buttons",
+                  "buttons": ["점심 메뉴(오띠모푸드)", "저녁 메뉴(오띠모푸드)"]
+              }
+          };
+          res.set({
+              'content-type': 'application/json'
+          }).send(JSON.stringify(res_object));
+        }
+    }
+
+    if (_obj.type == "text") {
+        if (_obj.content == "점심 메뉴(오띠모푸드)" || _obj.content == "저녁 메뉴(오띠모푸드)") {
+            if (_obj.content == "점심 메뉴(오띠모푸드)"){
+              setToday('ottimoJungsik');
+              console.log(today + ' 점심 메뉴(오띠모푸드) 클릭->' + _obj.user_key + '    img-->' + today_image_url);
+            }else{
+              setToday('ottimoSuksik');
+              console.log(today + ' 저녁 메뉴(오띠모푸드)->' + _obj.user_key + '    img-->' + today_image_url);
+            }
 
             fs.readFile(__dirname + '/crawl_data/' + today_text_name, 'utf-8', function(error, data) {
                 if (error) {
                     if (todayLabel == '토' || todayLabel == '일') {
-                        //ottimo_text = "주말은 식당 쉽니다." + "\n" + "월~금요일에 이용해주세요.";
                         out_ottimo_text = "주말은 식당 쉽니다." + "\n" + "월~금요일에 이용해주세요.";
                         today_image_url = "http://13.209.234.250:3000/holyday.jpg";
                     } else {
-                        //ottimo_text = '아직 ' + today_ottimo_message + " 메뉴가 공지되지 않았습니다. 오띠모푸드 중식 메뉴는 오전 11:00 ~ 12:00 사이에 업데이트 됩니다.";
                         out_ottimo_text = ottimo_text;
-                        //today_image_url = "http://13.209.234.250:3000/no_menu.jpg";
                     }
                 } else {
-                    //ottimo_text = data;
                     out_ottimo_text = data;
                     today_image_url = "http://13.209.234.250:3000/" + today_image_name;
                 }
                 out_today_image_url = today_image_url;
-            });
-            console.log(today + ' 오띠모푸드 클릭->' + _obj.user_key + '    img-->' + out_today_image_url);
 
-            res_object = {
-                "message": {
-                    "text": out_ottimo_text,
-                    "photo": {
-                        "url": out_today_image_url,
-                        "width": 720,
-                        "height": 630
+                res_object = {
+                    "message": {
+                        "text": out_ottimo_text,
+                        "photo": {
+                            "url": out_today_image_url,
+                            "width": 720,
+                            "height": 630
+                        },
+                        "message_button": {
+                            "label": "확대사진 입니다.",
+                            "url": out_today_image_url
+                        }
                     },
-                    "message_button": {
-                        "label": "확대사진 입니다.",
-                        "url": out_today_image_url
+                    "keyboard": {
+                        "type": "buttons",
+                        "buttons": ["오띠모푸드", "삼성웰스토리"]
                     }
-                },
-                "keyboard": {
-                    "type": "buttons",
-                    "buttons": ["오띠모푸드", "삼성웰스토리"]
-                }
-            };
-            res.set({
-                'content-type': 'application/json'
-            }).send(JSON.stringify(res_object));
+                };
+                res.set({
+                    'content-type': 'application/json'
+                }).send(JSON.stringify(res_object));
+            });
         }
 
         if (_obj.content == "삼성웰스토리") {
@@ -284,31 +301,31 @@ app.post('/message', function(req, res) {
                 },
                 "keyboard": {
                     "type": "buttons",
-                    "buttons": ["아침 메뉴", "점심 메뉴", "저녁 메뉴"]
+                    "buttons": ["아침 메뉴(웰스토리)", "점심 메뉴(웰스토리)", "저녁 메뉴(웰스토리)"]
                 }
             };
             res.set({
                 'content-type': 'application/json'
             }).send(JSON.stringify(res_object));
         }
-        if (_obj.content == "아침 메뉴") {
-            console.log(today + ' 아침 메뉴 클릭->' + _obj.user_key);
+        if (_obj.content == "아침 메뉴(웰스토리)") {
+            console.log(today + ' 아침 메뉴(웰스토리) 클릭->' + _obj.user_key);
             dbSelectBreakfast(function(msg) {
                 res.set({
                     'content-type': 'application/json'
                 }).send(JSON.stringify(msg));
             });
         }
-        if (_obj.content == "점심 메뉴") {
-            console.log(today + ' 점심 메뉴 클릭->' + _obj.user_key);
+        if (_obj.content == "점심 메뉴(웰스토리)") {
+            console.log(today + ' 점심 메뉴(웰스토리) 클릭->' + _obj.user_key);
             dbSelectLunch(function(msg) {
                 res.set({
                     'content-type': 'application/json'
                 }).send(JSON.stringify(msg));
             });
         }
-        if (_obj.content == "저녁 메뉴") {
-            console.log(today + ' 저녁 메뉴 클릭->' + _obj.user_key);
+        if (_obj.content == "저녁 메뉴(웰스토리)") {
+            console.log(today + ' 저녁 메뉴(웰스토리) 클릭->' + _obj.user_key);
             dbSelectDinner(function(msg) {
                 res.set({
                     'content-type': 'application/json'
